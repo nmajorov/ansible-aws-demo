c2293eb29b9558affd36d181a3bc6b735eb8a2f8
Nikolaj Majorov <nikolaj@majorov.biz>
2023-01-10 17:38:11 +0100

    add e2 instance creating

--- 001_deploy_ami_navigator_locally.zsh
+++ 001_deploy_ami_navigator_locally.zsh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH="`dirname \"$0\"`"
 ansible-navigator  run -v   $SCRIPT_PATH/001_deploy_ami_example.yaml  -m stdout \
---eev $HOME/.aws:/home/runner/.aws $@
+--eev $HOME/.aws:/home/runner/.aws --senv AWS_PROFILE=alex $@
 #\
 # \ --senv AWS_CONFIG_FILE="/home/runner/.aws/credentials"
 #--senv AWS_SHARED_CREDENTIALS_FILE="/home/runner/.aws/credentials"  $@
--- aws_useful_commmand.md
+++ aws_useful_commmand.md
@@ -2,3 +2,7 @@ show user id:
 
         export AWS_PROFILE=alex
         aws sts get-caller-identity
+
+vps:
+
+        aws ec2 describe-vpcs
