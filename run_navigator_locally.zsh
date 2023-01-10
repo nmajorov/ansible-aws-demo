faf7ad380b775314c13f4a570d496e1513f667d4
Nikolaj Majorov <nikolaj@majorov.biz>
2023-01-10 19:04:27 +0100

    add instances

--- run_navigator_locally.zsh
+++ run_navigator_locally.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
 SCRIPT_PATH="`dirname \"$0\"`"
-ansible-navigator  run -v   $SCRIPT_PATH/001_deploy_ami_example.yaml  -m stdout \
---eev $HOME/.aws:/home/runner/.aws --senv AWS_PROFILE=alex $@
+ansible-navigator  run -v $@   -m stdout \
+--eev $HOME/.aws:/home/runner/.aws --senv AWS_PROFILE=alex 
 #\
 # \ --senv AWS_CONFIG_FILE="/home/runner/.aws/credentials"
 #--senv AWS_SHARED_CREDENTIALS_FILE="/home/runner/.aws/credentials"  $@
