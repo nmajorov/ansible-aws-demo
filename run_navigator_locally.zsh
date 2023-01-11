25ee6f65edeb5b795627ca349a8c29e4e00ae989
Nikolaj Majorov <nikolaj@majorov.biz>
2023-01-11 14:03:09 +0100

    change navigator version

--- run_navigator_locally.zsh
+++ run_navigator_locally.zsh
@@ -2,7 +2,8 @@
 
 SCRIPT_PATH="`dirname \"$0\"`"
 ansible-navigator  run -v $@   -m stdout \
---eev $HOME/.aws:/home/runner/.aws --senv AWS_PROFILE=alex 
+--eev $HOME/.aws:/home/runner/.aws \
+--senv AWS_PROFILE=alex 
 #\
 # \ --senv AWS_CONFIG_FILE="/home/runner/.aws/credentials"
 #--senv AWS_SHARED_CREDENTIALS_FILE="/home/runner/.aws/credentials"  $@
